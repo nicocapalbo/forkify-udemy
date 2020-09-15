75eb3a8ee5418a0782b83636b9f95d30039f23ac
nicolas capalbo <capalbo.nicolas@gmail.com>
2020-09-15 12:27:26 -0300

    changing recipe servings + update values

--- src/js/index.js
+++ src/js/index.js
@@ -120,3 +120,19 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(e => { window.addEventListener(e, controlRecipe)});
 
+// recipe button clicks
+
+elements.resultRecipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServing('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        state.recipe.updateServing('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+})
+
+
